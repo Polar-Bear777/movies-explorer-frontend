9e151e2d628eced107615506de92f9eac150fb0d
Vladislav Alkhimenko <stormyaaa@yandex.ru>
2023-07-02 20:02:10 +0800

    добавил роуты и компонент Register

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,11 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Profile from '../Profile/Profile';
-import NotFound from '../NotFound/NotFound';
+// import Login from '../Login/Login';
+// import Movies from '../Movies/Movies';
+// import SavedMovies from '../SavedMovies/SavedMovies';
+// import Profile from '../Profile/Profile';
+// import NotFound from '../NotFound/NotFound';
 import Footer from '../Footer/Footer';
 import './App.css';
 
@@ -18,12 +18,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Register />} />
-        <Route path="/signin" element={<Login />} />
+        {/* <Route path="/signin" element={<Login />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/saved-movies" element={<SavedMovies />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Navigate to="/" replace/>} />
-        <Route path="/404" element={<NotFound />} />
+        <Route path="/404" element={<NotFound />} /> */}
       </Routes>
       <Footer/>
     </div>
