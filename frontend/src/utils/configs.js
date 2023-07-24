99c9f32e58c446468709952e82c68ad8f9fad812
Vladislav Alkhimenko <stormyaaa@yandex.ru>
2023-07-24 21:23:10 +0800

    Добавил компонент config

--- frontend/src/utils/configs.js
+++ frontend/src/utils/configs.js
@@ -4,8 +4,8 @@ export const apiMainConfig = {
     userData: 'https://api.bearpolar.nomoredomai.nomoreparties.sbs/users/me',
     defaultURL: 'https://api.bearpolar.nomoredomai.nomoreparties.sbs',
     signUp: 'https://api.bearpolar.nomoredomai.nomoreparties.sbs/signup',
-    signOut: 'https://api.bearpolar.nomoredomai.nomoreparties.sbs/signout',
     signIn: 'https://api.bearpolar.nomoredomai.nomoreparties.sbs/signin',
+    signOut: 'https://api.bearpolar.nomoredomai.nomoreparties.sbs/signout',
 }
 
 // СЕРВИС ДЛЯ ПОЛУЧЕНИЯ ФИЛЬМОВ
